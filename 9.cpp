b2af1b84c67c64205ae7720d9095f9190d39cd1d
batyrkhan <93762843+bsapiev@users.noreply.github.com>
2021-11-07 21:56:29 +0600

    Create 9.cpp

---
+++ 9.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+using namespace std; 
+int main(){ 
+    int b, c, n; 
+    cin >> b >> c >> n; 
+    cout << b * n + (c * n) / 100 << " " << (c * n) % 100;
+   
+ 
+    return 0;
+} 

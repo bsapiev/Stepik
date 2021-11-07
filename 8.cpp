0b9be839124bca6c9f1168a28e913768f33cc976
batyrkhan <93762843+bsapiev@users.noreply.github.com>
2021-11-07 21:56:05 +0600

    Create 8.cpp

---
+++ 8.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+using namespace std; 
+int main(){ 
+    int a, b, c, d, e, f;  
+    cin >> a >> b >> c; 
+    d = (a + 2 - 1) / 2; 
+    e = (b + 2 - 1) / 2; 
+    f = (c + 2 - 1) / 2; 
+    cout << d + e + f;
+    return 0;
+} 
